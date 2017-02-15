996ceddeba4c4f0e6568299d8fa9ebfaf735bef8
Oz <o.lodriguez@gmail.com>
2017-02-15 13:21:48 +0100

    generate random figures higherorder

--- src/modules/drawing/engine.draw.ts
+++ src/modules/drawing/engine.draw.ts
@@ -4,29 +4,34 @@ export class Draw extends Canvas {
 
     public constructor() {
         super(window.innerWidth, window.innerHeight);
-        
+
     }
 
     public drawRectangle() {
         console.log('should draw rectangle');
-        super.context.strokeRect(Math.random() * super.canvasWidth * 0.8, 
-        Math.random() * super.canvasHeight * 0.8, 
-        Math.random() * 30 + 10, Math.random() * 30 + 10);
+        super.context.strokeRect(
+            this.generateRandomPosition(super.canvasWidth, 0.8),
+            this.generateRandomPosition(super.canvasHeight, 0.8),
+            this.generateRandomPosition(2, 40),
+            this.generateRandomPosition(4, 40)
+        );
     }
 
     public drawCircle() {
         console.log('should draw circle');
         super.context.beginPath();
-        super.context.arc(Math.random() * super.canvasWidth * 0.8,
-        Math.random() * this.canvasHeight * 0.8,
-        Math.random() * 30 + 10, 0, Math.PI * 2, true);
+        super.context.arc(
+            this.generateRandomPosition(super.canvasWidth, 0.8),
+            this.generateRandomPosition(super.canvasHeight, 0.8),
+            this.generateRandomPosition(2, 40),
+            0, Math.PI * 2, true);
 
         super.context.closePath();
         super.context.stroke();
     }
 
-    private generateRandomPosition(canvasDimension: number, randomnessThreshold: number) {
-        console.log('should return random position');
+    private generateRandomPosition(canvasDimension: number, randomnessThreshold: number): number {
+        return Math.random() * canvasDimension * randomnessThreshold;
     }
 
 }
