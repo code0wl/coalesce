d12c1a2f5689b89534f318ac94d6f9b27a1d1f53
code0wl <o.lodriguez@gmail.com>
2018-06-09 19:52:08 +0200

    lint

--- lib/src/models/shape/rectangle.ts
+++ lib/src/models/shape/rectangle.ts
@@ -1,7 +1,9 @@
+import { Vector } from '../../module/components/vector/engine.vector';
+
 export interface Rectangle {
     rigidShapeType: string;
     height: number;
     width: number;
-    vertexes: Array<any>;
-    compass: Array<any>;
+    vertexes: Array<Vector>;
+    compass: Array<Vector>;
 }
