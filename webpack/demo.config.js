1103b281e1ff657fe506f0b726125de9edb46568
Oscar Lodriguez <o.lodriguez@gmail.com>
2019-09-20 16:32:30 +0200

    pre-rewerite

--- webpack/demo.config.js
+++ webpack/demo.config.js
@@ -2,28 +2,27 @@ const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
-    entry: path.resolve(__dirname, '../demo/game.ts'),
-    output: {
-        path: path.resolve(__dirname, '../demo/dist'),
-        filename: './bundle.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                loader: 'ts-loader',
-                options: {
-                    transpileOnly: true
-                }
-            }
-        ]
-    },
-    plugins: [
-        new UglifyJsPlugin()
+  entry: path.resolve(__dirname, '../demo/game.ts'),
+  watch: true,
+  output: {
+    path: path.resolve(__dirname, '../demo/dist'),
+    filename: './bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        }
+      }
     ]
+  },
+  plugins: [new UglifyJsPlugin()]
 };
 
 module.exports = config;
